54f83245d1f1f69bf5875f80f17b2120d2805e02
curly_fries <srushtikalambe50@gmail.com>
2023-12-15 03:57:45 -0800

    assignment 2-Python basics

---
+++ assignment_2.py
@@ -0,0 +1,66 @@
+# Question no.1 :-
+# to comment a code in python we use "#" before writing any single line comment for example:-
+#assigning a value to a
+a=12
+#print it
+print(a)
+
+#and for a multiple line coment we use ("""  """) for example:-
+"""hello, how are you? I am fine, wassup?"""
+
+# question no.2:-
+#variables in python means to which we assign value which get saved by system and very variable have unique id for example:-
+strng="hello world"
+print(strng)
+# so strng is a variable and hello world is a value assigned to that variable
+#variables are of types like integer,string,boolen,float etc.
+
+# Question no. 3:-
+intger=2 #data type=integer converting to string
+b=str(intger)
+print(type(b))
+flot=22.3 #data tpe= float converting to integer
+c=int(flot)
+print(type(c))
+#To convert any data type use the function as per your need for ex. int(), str()for example the above code 
+
+# Question no. 4:-
+
+
+#Question no. 5:-
+my_list=[1,2,3,4,5]
+sub_list=[my_list[1:3]]
+print(sub_list) 
+
+# Question no. 6:_
+# In maths complex number is a number which can be expressed in the form of a+bi where 'a' & 'b' are real numbers and i is a complex number.
+# in python the complex number is a inbuilt syntax it is represented as a+bj where a is real part and bj is a imaginary part.
+#for ex.
+a=1+5j
+print(type(a))
+
+# Question no. 7:-
+#correct way to assign a value to variable 
+age=25
+print(age)
+
+# Question no. 8:-
+price=9.99
+print(type(price))
+
+# Question no. 9:-
+name="Srushti vitthal kalambe"
+print(name)
+
+# Question no. 10:-
+a="Hello World!"
+b=a[6:12]
+print(b)
+
+# Question no. 11 :-
+is_student=True
+print(is_student)
+
+
+
+
